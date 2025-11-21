825e1fb6d5d99d15d259b64b3ed68dea5db6d443
jeanne <jeanne@tenko.ai>
2025-11-21 16:55:47 +0100

    updated comments

--- day10.py
+++ day10.py
@@ -12,13 +12,13 @@ total_score = 0
 
 from collections import deque
 
-# at each increment update list of coordinates and height, check if former & latter match and
+# at each increment update list of coordinates and height, check if latter & value of former match, find
 ## Part 1: unique positions of 9
 ## Part 2: unique paths that reach 9
 for start_row, start_col in trailheads:
 
     """ Part 1:
-    queue =  deque([(start_row, start_col, 0)])  #current_height initialised at 0 at trailhead
+    queue =  deque([(start_row, start_col, 0)])  #height initialised at 0 at trailhead
     visited = set()
     nines_reached = set() """
     # list of tuples of coordinates and height PLUS path (Part 2)
@@ -26,8 +26,8 @@ for start_row, start_col in trailheads:
     paths_to_nine = set()
     
     while queue:
-        #row, col, current_height = queue.popleft() #Part 1
-        row, col, current_height, path = queue.popleft()
+        #row, col, height = queue.popleft() #Part 1
+        row, col, height, path = queue.popleft()
 
         # skip processing of visited positions (Part 1)
         #if (row, col) in visited:
@@ -40,16 +40,16 @@ for start_row, start_col in trailheads:
             paths_to_nine.add(tuple(path))
             continue
         
-        # iterate through 4 directions, if within grid & single increment repeat iteration, and update list
-        next_height = current_height + 1
+        # iterate through 4 directions, if within grid & single increment repeat iteration, update list
+        new_height = height + 1
         for r, c in directions:
             new_row, new_col = row + r, col + c
             
             if 0 <= new_row < len(lines) and 0 <= new_col < len(lines[0]):
-                if lines[new_row][new_col] == str(next_height):
-                    #queue.append((new_row, new_col, next_height)) #Part 1
+                if lines[new_row][new_col] == str(new_height):
+                    #queue.append((new_row, new_col, new_height)) #Part 1
                     new_path = path + [(new_row, new_col)]
-                    queue.append((new_row, new_col, next_height, new_path))
+                    queue.append((new_row, new_col, new_height, new_path))
     
     #total_score += len(nines_reached) #Part 1: get number of unique 9's reached
     total_score += len(paths_to_nine) #Part 2: get distinct paths to any 9
